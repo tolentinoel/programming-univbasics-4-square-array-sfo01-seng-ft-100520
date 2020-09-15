8840e04a63aaa3900059949b348ec1c896d1052e
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-15 20:31:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,10 @@
 def square_array(array)
   new_array = []
   
-  array.length.times { |index|
-  new_array.push(array[index])
-  }
-  puts new_array
+  array.length.times do |index|
+  new_array.push(array[index] + 1)
+end
+new_array
 end
 
 puts square_array([2,9])
